e5fb45de49a52a605e370a1460cfb3dae198a0d8
igor <ilaborie@gmail.com>
2016-11-10 07:03:07 +0100

    remove swagger

--- src/main/java/org/ilaborie/cat/App.java
+++ src/main/java/org/ilaborie/cat/App.java
@@ -2,11 +2,8 @@ package org.ilaborie.cat;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.ilaborie.cat.Cat.CatRace;
-import org.jooby.Err;
-import org.jooby.Jooby;
-import org.jooby.Results;
+import org.jooby.*;
 import org.jooby.json.Jackson;
-import org.jooby.swagger.SwaggerUI;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -21,108 +18,75 @@ import static org.jooby.Status.NOT_FOUND;
 
 public class App extends Jooby {
 
-  {
-    // JSON mapping
-    ObjectMapper mapper = new ObjectMapper()
-        .setVisibility(GETTER, NON_PRIVATE)
-        .setVisibility(SETTER, NON_PRIVATE);
-    use(new Jackson(mapper));
+    {
+        // JSON mapping
+        ObjectMapper mapper = new ObjectMapper()
+                .setVisibility(GETTER, NON_PRIVATE)
+                .setVisibility(SETTER, NON_PRIVATE);
+        use(new Jackson(mapper));
 
-    /**
-     *
-     * Everything about your Cats.
-     */
-    use("/api/cats")
-        /**
-         *
-         * List cats ordered by id.
-         *
-         * @return Cats ordered by name.
-         */
-        .get(req -> {
-          DB db = req.require(DB.class);
-          return db.findAll();
-        })
-        /**
-         *
-         * Find cat by ID
-         *
-         * @param id Cat ID.
-         * @return Returns <code>200</code> with a single cat or <code>404</code>
-         */
-        .get("/:id", req -> {
-          String id = req.param("id").value();
-          DB db = req.require(DB.class);
-          Cat cat = db.find(id);
-          if (cat == null) {
+        use("/api/cats")
+                .get(this::findAll)
+                .get("/:id", this::findById)
+                .post(this::create)
+                .put(this::update)
+                .delete("/:id", req -> this.delete(req))
+                .produces("json")
+                .consumes("json");
+
+        // Create felines herd
+        onStart(registry -> {
+            DB db = registry.require(DB.class);
+            List<CatRace> races = Arrays.asList(CatRace.values());
+            Supplier<CatRace> randomRace = () -> {
+                Collections.shuffle(races);
+                return races.get(0);
+            };
+            // every decent cat have an Egyptian God name
+            Stream.of("Isis", "Nut", "Anubis", "Osiris", "Thoth")
+                    .map(name -> new Cat(name, randomRace.get()))
+                    .forEach(db::save);
+        });
+    }
+
+    public static void main(final String[] args) throws Exception {
+        new App().start(args);
+    }
+
+    private List<Cat> findAll(Request req) {
+        DB db = req.require(DB.class);
+        return db.findAll();
+    }
+
+    private Cat findById(Request req) {
+        String id = req.param("id").value();
+        DB db = req.require(DB.class);
+        Cat cat = db.find(id);
+        if (cat == null) {
             throw new Err(NOT_FOUND);
-          }
-          return cat;
-        })
-        /**
-         *
-         * Add a new cat to the store.
-         *
-         * @param body Cat object that needs to be added to the store.
-         * @return Returns a saved cat.
-         */
-        .post(req -> {
-          Cat cat = req.body(Cat.class);
-          DB db = req.require(DB.class);
-          db.save(cat);
-          return cat;
-        })
-        /**
-         *
-         * Update an existing cat.
-         *
-         * @param body Cat object that needs to be updated.
-         * @return Returns a saved cat.
-         */
-        .put(req -> {
-          Cat cat = req.body(Cat.class);
-          DB db = req.require(DB.class);
-          db.save(cat);
-          return cat;
-        })
-        /**
-         *
-         * Deletes a cat by ID.
-         *
-         * @param id Cat ID.
-         * @return A <code>204</code>
-         */
-        .delete("/:id", req -> {
-          String id = req.param("id").value();
-          DB db = req.require(DB.class);
-          db.delete(id);
-          return Results.noContent();
-        })
-        .produces("json")
-        .consumes("json");
+        }
+        return cat;
+    }
 
-    // Provide Swagger
-    new SwaggerUI()
-        .tag(route -> "cats")
-        .install(this);
+    private Cat create(Request req) throws Exception {
+        Cat cat = req.body(Cat.class);
+        DB db = req.require(DB.class);
+        db.save(cat);
+        return cat;
+    }
 
-    // Create felines herd
-    onStart(registry -> {
-      DB db = registry.require(DB.class);
-      List<CatRace> races = Arrays.asList(CatRace.values());
-      Supplier<CatRace> randomRace = () -> {
-        Collections.shuffle(races);
-        return races.get(0);
-      };
-      // every decent cat have an Egyptian God name
-      Stream.of("Isis", "Nut", "Anubis", "Osiris", "Thoth")
-          .map(name -> new Cat(name, randomRace.get()))
-          .forEach(db::save);
-    });
-  }
+    private Cat update(Request req) throws Exception {
+        Cat cat = req.body(Cat.class);
+        DB db = req.require(DB.class);
+        db.save(cat);
+        return cat;
+    }
 
-  public static void main(final String[] args) throws Exception {
-    new App().start(args);
-  }
+    private Result delete(Request req) {
+        String id = req.param("id").value();
+        DB db = req.require(DB.class);
+        db.delete(id);
+        return Results.noContent();
+    }
 
 }
--- src/main/java/org/ilaborie/cat/DB.java
+++ src/main/java/org/ilaborie/cat/DB.java
@@ -1,10 +1,9 @@
 package org.ilaborie.cat;
 
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
-
 import javax.inject.Singleton;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -13,25 +12,23 @@ import java.util.stream.Collectors;
 @Singleton
 public class DB {
 
-  private Cache<String, Cat> db = CacheBuilder.newBuilder()
-      .maximumSize(100)
-      .build();
+    private Map<String, Cat> db = new HashMap<>();
 
-  public List<Cat> findAll() {
-    return db.asMap().values().stream()
-        .collect(Collectors.toList());
-  }
+    public List<Cat> findAll() {
+        return db.values().stream()
+                .collect(Collectors.toList());
+    }
 
-  public Cat find(String id) {
-    return db.getIfPresent(id);
-  }
+    public Cat find(String id) {
+        return db.get(id);
+    }
 
-  public void save(Cat cat) {
-    db.put(cat.getId(), cat);
-  }
+    public void save(Cat cat) {
+        db.put(cat.getId(), cat);
+    }
 
-  public void delete(String id) {
-    db.invalidate(id);
-  }
+    public void delete(String id) {
+        db.remove(id);
+    }
 
 }
